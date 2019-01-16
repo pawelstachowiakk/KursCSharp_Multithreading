5a54e8b83b46ab7593ac63b184e1c0d8fe7c1b54
Pawel <p.stachowiak@xberry.tech>
2019-01-16 16:13:38 +0100

    Add project files.

---
+++ 1 CreatingFirstThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_CreatingFirstThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
